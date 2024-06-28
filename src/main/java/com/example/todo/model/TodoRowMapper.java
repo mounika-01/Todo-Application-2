1c8bd48dcb9b3f84d59fad080296e788f9705d16
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-06-28 09:51:31 +0530

    Update TodoRowMapper.java

--- src/main/java/com/example/todo/model/TodoRowMapper.java
+++ src/main/java/com/example/todo/model/TodoRowMapper.java
@@ -1,19 +1,22 @@
 package com.example.todo.model;
 
+
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 import com.example.todo.model.Todo;
+
 
 public class TodoRowMapper implements RowMapper<Todo>{
 
+
     public Todo mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Todo{
+        return new Todo(
             rs.getInt("id"),
             rs.getString("todo"),
             rs.getString("priority"),
-            rs.getstring("status")
-        };
+            rs.getString("status"));
     }
 }
